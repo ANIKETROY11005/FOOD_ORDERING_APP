dfae03e22b36f7b18090a3c803e040d4a6f39af2
ANIKETROY11005 <draxnme12345@gmail.com>
2023-03-27 11:57:45 +0530

    Added mealItem, card, Cart, input

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React, { Fragment } from "react";
 import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
 function App() {
   return (
     <Fragment>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   );
 }
