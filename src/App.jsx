e6cee1207f3d4b3b879e8c5aa83e433ba82728eb
ANIKETROY11005 <draxnme12345@gmail.com>
2023-03-27 10:58:08 +0530

    Added header and cart component

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+import Header from "./components/Layout/Header";
+function App() {
+  return (
+    <Fragment>
+      <Header />
+    </Fragment>
+  );
+}
+
+export default App;
