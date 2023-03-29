d0ce19aafcc2a3ae0dee500e0c2199f9ebee2a99
ANIKETROY11005 <draxnme12345@gmail.com>
2023-03-29 12:51:31 +0530

    added dynamic Cart component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  }
+
+  const hideCardHandler = () => {
+    setCartIsShown(false);
+  }
+
   return (
     <Fragment>
-      <Header />
+      {cartIsShown && <Cart onClose={hideCardHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,21 +1,22 @@
 import classes from './Cart.module.css';
+import Modal from '../UI/Modal';
 
-const Card = props => {
-    const cartItems = <ul className={classes['cart-items']}>{[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }
+const Cart = props => {
+    const cartItems = <ul className={classes['cart-items']}>{[{ key: '1', id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }
     ].map(item => <li>{item.name}</li>)}</ul>;
     return (
-        <div>
+        <Modal onClose={props.onClose}>
             {cartItems}
             <div className={classes.total}>
                 <span>Total Amount</span>
                 <span>35.62</span>
             </div>
             <div className={classes.action}>
-                <button className={classes['button--alt']}>close</button>
+                <button className={classes['button--alt']} onClick={props.onClose}>close</button>
                 <button className={classes.button}>Order</button>
             </div>
-        </div>
+        </Modal>
     );
 };
 
-export default Card;
+export default Cart;
